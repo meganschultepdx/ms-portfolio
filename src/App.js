0083c2e29d4cc6189212c6c56f67fe65aa18a507
megan schulte <megan.schulte.pdx@gmail.com>
2019-10-07 16:32:11 -0700

    add project component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import HeaderT from "./Components/HeaderT";
 import About from "./Components/About";
+import Projects from "./Components/Projects";
 
 function App() {
 	return (
 		<div className="App">
 			<HeaderT />
 			<About />
+			<Projects />
 		</div>
 	);
 }
