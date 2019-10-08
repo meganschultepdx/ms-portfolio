96f87fa9f292afff1c4226ff4519fc36f67a2eaa
megan schulte <megan.schulte.pdx@gmail.com>
2019-10-08 15:12:29 -0700

    add google fonts

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -11,16 +11,16 @@ const Contact = () => {
 	return (
 		<div className="contact">
 			<a href="mailto:megan.schulte.pdx@gmail.com">
-				<FontAwesomeIcon icon={faEnvelopeSquare} size="lg" />
+				<FontAwesomeIcon icon={faEnvelopeSquare} size="2x" target="blank" />
 			</a>
 			<a href="https://www.linkedin.com/in/megan-schulte-pdx/">
-				<FontAwesomeIcon icon={faLinkedin} size="lg" />
+				<FontAwesomeIcon icon={faLinkedin} size="2x" target="blank" />
 			</a>
 			<a href="https://github.com/meganschultepdx">
-				<FontAwesomeIcon icon={faGithubSquare} size="lg" />
+				<FontAwesomeIcon icon={faGithubSquare} size="2x" target="blank" />
 			</a>
 			<a href="https://twitter.com/MeganSchultePdx?s=09">
-				<FontAwesomeIcon icon={faTwitterSquare} size="lg" />
+				<FontAwesomeIcon icon={faTwitterSquare} size="2x" target="blank" />
 			</a>
 		</div>
 	);
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -11,32 +11,30 @@ const Projects = () => {
 	return (
 		<div className="projects">
 			<h2 className="sectionLabel">projects</h2>
-			<h3 className="projectTitles">partner scraper tool</h3>
-			<div className="imageBackground">
-				<div className="whiteImageBackground">
-					<img
-						src={PartnerScraperPic}
-						className="screenShot"
-						alt="screen shots of app"
-					/>
-				</div>
-			</div>
+
+			<h3 className="projectTitles">the stephen kingdom</h3>
+			<img
+				src={stephenkingdom}
+				alt="screen shots of app"
+				className="screenShot"
+			/>
+			<h4 className="stackUsed">React Native App with Firebase as BaaS</h4>
 			<p className="aboutProject">
-				This was an internal company tool created during a five week internship.
-				The back-end is built as an independent API with C#/EF Core/Sqlite; the
-				front-end is built in React with Hooks and Typescript and makes get
+				This mobile app will be a database for information on Stephen King
+				related movies and mini-series. It will provide users with information
 			</p>
 			<a
-				href="https://github.com/meganschultepdx/Epicodus-Internship-Project"
+				href="https://github.com/meganschultepdx/the-stephen-kingdom"
 				target="blank"
 				className="linkToCode"
 			>
 				view Code
 			</a>
+			<div className="spacer"></div>
 
 			<h3 className="projectTitles">highlander tap house</h3>
 			<img src={highlander} alt="screen shots of app" className="screenShot" />
-
+			<h4 className="stackUsed">React web app</h4>
 			<p className="aboutProject">
 				This web application is designed to be both a tap room beer list for
 				customers and a keg tracking app for the eployees of said tap room. It
@@ -49,39 +47,50 @@ const Projects = () => {
 			>
 				view Code
 			</a>
+			<div className="spacer"></div>
 
-			<h3 className="projectTitles">the stephen kingdom</h3>
-			<img
-				src={stephenkingdom}
-				alt="screen shots of app"
-				className="screenShot"
-			/>
+			<h3 className="projectTitles">partner scraper tool</h3>
+			<div className="imageBackground">
+				<div className="whiteImageBackground">
+					<img
+						src={PartnerScraperPic}
+						className="screenShot"
+						alt="screen shots of app"
+					/>
+				</div>
+			</div>
+			<h4 className="stackUsed">
+				react with hooks & c#/EF Core/Sqlite web app
+			</h4>
 			<p className="aboutProject">
-				This mobile app will be a database for information on Stephen King
-				related movies and mini-series. It will provide users with information
+				This was an internal company tool created during a five week internship.
+				The back-end is built as an independent API with C#/EF Core/Sqlite; the
+				front-end is built in React with Hooks and Typescript and makes get
 			</p>
 			<a
-				href="https://github.com/meganschultepdx/the-stephen-kingdom"
+				href="https://github.com/meganschultepdx/Epicodus-Internship-Project"
 				target="blank"
 				className="linkToCode"
 			>
 				view Code
 			</a>
+			<div className="spacer"></div>
 
 			<h3 className="projectTitles">portland field guide</h3>
 			<img src={fieldguide2} alt="screen shots of app" className="screenShot" />
-			<a
-				href="https://portland-field-guide.firebaseapp.com/"
-				target="blank"
-				className="linkToCode"
-			>
-				view hosted site
-			</a>
+			<h4 className="stackUsed">Angular & Firebase web app</h4>
 			<p className="aboutProject">
 				This is a web application that allows users to search local native and
 				invasive plants in Portland, OR. This app was inspired by the love of
 				plants and the desire to know them better. It is very satisfying to be
 			</p>
+			<a
+				href="https://portland-field-guide.firebaseapp.com/"
+				target="blank"
+				className="linkToHosted"
+			>
+				view hosted site
+			</a>
 			<a
 				href="https://github.com/meganschultepdx/Portland-field-guide"
 				target="blank"
@@ -89,9 +98,11 @@ const Projects = () => {
 			>
 				view Code
 			</a>
+			<div className="spacer"></div>
 
 			<h3 className="projectTitles">alchemy hair salon</h3>
 			<img src={hairsalon} alt="screen shots of app" className="screenShot" />
+			<h4 className="stackUsed">c# & MySql website</h4>
 			<p className="aboutProject">
 				This web application allows the user to check the surf report and
 				weather for three main surf spots on the Oregon coast. It prpovides only
@@ -105,21 +116,23 @@ const Projects = () => {
 			>
 				view Code
 			</a>
+			<div className="spacer"></div>
 
 			<h3 className="projectTitles">pdx free food finder</h3>
 			<img src={freefood} alt="screen shots of app" className="screenShot" />
-			<a
-				href="https://meganschultepdx.github.io/free-food-finder/index.html"
-				target="blank"
-				className="linkToCode"
-			>
-				view hosted site
-			</a>
+			<h4 className="stackUsed">JavaScript & JQuery Website</h4>
 			<p className="aboutProject">
 				This is a website that helps users easily locate free food-pantries and
 				meals across Portland, OR. As such, we call it "Free Food Finder PDX"
 				(FFF-PDX). The landing page displays the FFF-PDX logo, a menu bar and a
 			</p>
+			<a
+				href="https://meganschultepdx.github.io/free-food-finder/index.html"
+				target="blank"
+				className="linkToHosted"
+			>
+				view hosted site
+			</a>
 			<a
 				href="https://github.com/meganschultepdx/free-food-finder"
 				target="blank"
@@ -127,6 +140,7 @@ const Projects = () => {
 			>
 				view Code
 			</a>
+			<div className="spacer"></div>
 		</div>
 	);
 };
