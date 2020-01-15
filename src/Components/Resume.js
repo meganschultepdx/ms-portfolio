9fbbfce6119c14f1775d18713a7ea023893f0b74
megan schulte <megan.schulte.pdx@gmail.com>
2020-01-14 21:22:06 -0800

    update back button on resume page

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -7,7 +7,9 @@ const Resume = () => {
 	return (
 		<React.Fragment>
 			<Link to="/">
-				<button className="back-home">back home</button>
+				<div className="backhome-container ">
+					<button className="back-home">back home</button>
+				</div>
 			</Link>
 			<PDFViewer backend={PDFJSBackend} src="/megan_schulte_resume.pdf" />
 		</React.Fragment>
