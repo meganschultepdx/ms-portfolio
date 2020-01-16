72685412714888eef7acff835a8914e97b53b117
megan schulte <megan.schulte.pdx@gmail.com>
2020-01-16 11:44:18 -0800

    add scroll button to resume component

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -2,16 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PDFViewer from "./PDXViewer";
 import PDFJSBackend from "../Backends/pdfjs";
+import ScrollButton from "./ScrollButton";
 
 const Resume = () => {
 	return (
 		<React.Fragment>
-			<Link to="/">
+			<Link to="/" className="home-link">
 				<div className="backhome-container ">
 					<button className="back-home">back home</button>
 				</div>
 			</Link>
 			<PDFViewer backend={PDFJSBackend} src="/megan_schulte_resume.pdf" />
+			<ScrollButton />
 		</React.Fragment>
 	);
 };
